917aede2663e4aee55033db0503f1c21769eada9
twedella <tamyres_wedella@hotmail.com>
2025-08-01 13:50:24 -0300

    Inicializando o projeto

---
+++ src/main/java/com/TTecnologia/RURL/RurlApplication.java
@@ -0,0 +1,13 @@
+package com.TTecnologia.RURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TTecnologia/RURL/RurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TTecnologia.RURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
